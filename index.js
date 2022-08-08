800461a99c22f77b79676927bb81fefb9f00daad
tuyizerejean <tuyibobu@gmail.com>
2022-08-08 13:20:13 -0400

    using mongoose

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express =require('express');
+const bodyParser=require("body-parser");
+const mongoose=require('mongoose');
 
-const router= express.Router();
-router.post("/",(req,res)=>{
-    const {sessionId,serviceCode,phoneNumber,text}=req.body;
-    console.log('###############',req.body);
-    let response="";
+const app= express();
+
+const PORT=800;
+
+const database_url='';
+mongoose.connect(database_url);
+const db=mongoose.connection;
+db.on('error',(err)=>{
+    console.log(err);
 })
+db.once('open',()=>{
+    console.log('database is running .')
+})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.get('/',(req,res)=>{
+    res.send('Success Message');
+})
+
+
+
+app.listen(PORT,()=>{
+   console.log(`app is running on the port$PORT`); 
+});
+
+// router.post("/",(req,res)=>{
+//     const {sessionId,serviceCode,phoneNumber,text}=req.body;
+//     console.log('###############',req.body);
+//     let response="";
+// })
 
 
